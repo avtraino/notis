fdc44f54e42cbdd33652ed55fa30c3de338b445d
Vito Traino <avtraino@gmail.com>
2018-08-30 13:01:23 -0400

    change prod check

--- notify.py
+++ notify.py
@@ -5,7 +5,7 @@ from secrets import noti_from, noti_pass, noti_to
 
 def prod_only(func):
     def wrapper(*args, **kwargs):
-        if 'Linux' in platform.platform():
+        if '-gcp-' in platform.platform():
             func(*args, **kwargs)
     return wrapper
 
