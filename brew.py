8e4bf4a781dc78ed14d043cfb2dcdf6a038dd9dc
Vito Traino <avtraino@gmail.com>
2018-09-20 23:56:07 -0400

    add new beer if not in beer list

--- brew.py
+++ brew.py
@@ -8,34 +8,52 @@ logging.basicConfig(filename=secrets.logfile, level=logging.INFO,
                     datefmt="%Y-%m-%d %H:%M:%S")
 
 def top_rated():
+    # open data file
     datafile = 'brew_data/top_rated.json'
     with open(datafile, 'r') as brew:
         data = json.load(brew)
-        print("Old Data: ", data)
         recent = data['recent']
-
+        beer_list = data['beer_list']
+    
+    # get new checkins
     key = secrets.untappd_token
     endpoint = 'https://api.untappd.com/v4/checkin/recent?limit=50&min_id='+recent+'&access_token='
     link = endpoint + key 
     print(link)
     res = requests.get(link)
-    output = res.text
-    print(output)
+    # output = res.text
+    # print(output)
     block = res.json()
+
+
+    # if new checkins, do work
     if block['response']['checkins']['count'] > 0:
 
-        try:
-            new_recent = block['response']['checkins']['items'][0]['checkin_id']
-            data['recent'] = str(new_recent)
-        except IndexError:
-            pass
-            
-        # data['beer_list'].append(add_beer)
+        # new_recent = block['response']['checkins']['items'][0]['checkin_id']
+        # data['recent'] = str(new_recent)
+        
 
+        current_bids = [i['bid'] for i in beer_list]
+        checkins = block['response']['checkins']['items']
+
+        for i in checkins:
+            name = i['user']['first_name'] + " " + i['user']['last_name']
+            beer = i['beer']['beer_name']
+            bid = str(i['beer']['bid'])
+            brewery = i['brewery']['brewery_name']
+            rating = i['rating_score']
+            if rating >= 4.5:
+                print(name , "gave the", beer , "by" , brewery , str(rating) , "stars")
+                new_beer = {"bid":bid, "name":beer}
+                if new_beer['bid'] not in current_bids:
+                    beer_list.append(new_beer)
+
+
+
+        # write new data 
         with open(datafile, 'w') as brew:
             json.dump(data, brew)
 
-        print("New Data: ", data)
     else:
         logging.info("no new checkins")
 
