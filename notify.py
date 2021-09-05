8f9200c26ad3ed69581102270a8a8e0a29533661
Vito Traino <avtraino@gmail.com>
2021-09-05 17:41:08 -0400

    enable sending arbitrary email

--- notify.py
+++ notify.py
@@ -1,7 +1,7 @@
 import smtplib, platform
 from email.message import EmailMessage
 from secrets import noti_from, noti_pass, noti_default_to
-
+import sys, argparse
 
 def prod_only(func):
     def wrapper(*args, **kwargs):
@@ -21,3 +21,16 @@ def send_email(subject, body=" ", send_to=noti_default_to):
     server.login( noti_from, noti_pass )
     server.send_message(msg)
     server.quit()
+
+
+if __name__ == "__main__":
+
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--subject')
+    parser.add_argument('--body')
+    args=parser.parse_args()
+
+    subject = args.subject
+    body = args.body
+
+    send_email(subject, body)
