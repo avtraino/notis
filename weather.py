beb72bf1b8aa781bc36b44ccbfc8966ab63ac93d
Vito Traino <avtraino@gmail.com>
2021-09-06 22:11:06 -0400

    update weather api v4

--- weather.py
+++ weather.py
@@ -9,33 +9,33 @@ logging.basicConfig(filename=secrets.logpath+'weather.log', level=logging.INFO,
                     datefmt="%Y-%m-%d %H:%M:%S")
 
 next_nine_hours = ( datetime.utcnow() + timedelta(hours=9) ).strftime('%Y-%m-%dT%H:%M:%SZ')
-bad_codes = ["rain_heavy", "snow_heavy", "freezing_rain_heavy", "tstorm"]
+bad_codes = {4201: "Heavy Rain", 5101: "Heavy Snow", 6000: "Freezing Drizzle", 6001: "Freezing Rain", 6201: "Heavy Freezing Rain", 8000: "Thunderstorm"}
     
 def heavy_rain():
-    url = "https://api.climacell.co/v3/weather/forecast/hourly"
+    url = "https://api.tomorrow.io/v4/timelines"
     querystring = {   # defaults: unit_system = si
-        "lat" : '37.557', "lon" : '-77.475', 
-        "start_time" : "now", "end_time" : next_nine_hours, 
-        "fields" : "precipitation,weather_code,precipitation_probability",
-        "apikey" : secrets.clima_key}
+        "location" : '37.557 ,-77.475', 
+        "endTime" : next_nine_hours, 
+        "fields" : "precipitationIntensity,precipitationProbability,weatherCode",
+        "apikey" : secrets.tomorrow_key}
 
     res = httpx.get(url, params=querystring)
     block = res.json()
 
     bad_hours, bad_hour_count = "", 0
-    for hour in block:
-        utc_string = hour['observation_time']['value']
-        probability = hour['precipitation_probability']['value']
-        code = hour['weather_code']['value']
-        precip = round(hour['precipitation']['value'],1)
-        logging.debug(f"code: {code} -- precip: {precip} mm/hr -- prob: {probability}% chance")
-        if (probability >= 25) and (code in bad_codes): 
-            local_stamp = datetime.strptime(utc_string, '%Y-%m-%dT%H:%M:%S.000Z').replace(tzinfo=timezone.utc).astimezone(tz=None)
+    for hour in block['data']['timelines'][0]['intervals']:
+        utc_string = hour['startTime']
+        probability = hour['values']['precipitationProbability']
+        code = hour['values']['weatherCode']
+        precip = round(hour['values']['precipitationIntensity'],1)
+        if (probability >= 25) and (code in bad_codes.keys()):
+            weather_str = bad_codes[code]
+            local_stamp = datetime.strptime(utc_string, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc).astimezone(tz=None)
             nice_time = local_stamp.strftime("%I%p")
             bad_hour_count = bad_hour_count + 1
-            bad_hours = bad_hours + f"{nice_time} -- {code} -- {precip} mm/hr -- {probability}% chance \n"
+            bad_hours = bad_hours + f"{nice_time} -- {weather_str} -- {precip} mm/hr -- {probability}% chance \n"
 
-
+            
     if bad_hour_count > 0:
         subject = f"Weather Alert: {bad_hour_count} hours with heavy precipitation"
         body = bad_hours
@@ -54,4 +54,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
